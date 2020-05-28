20b64a67dd805ec70a2722e3184205bde8d99443
FireHeart003 <65786491+FireHeart003@users.noreply.github.com>
2020-05-28 19:57:53 -0400

    Add files via upload

---
+++ ProGen/Main.java
@@ -0,0 +1,26 @@
+package ProGen;
+
+//import java.net.URL;
+
+//import javax.sound.sampled.AudioInputStream;
+//import javax.sound.sampled.AudioSystem;
+//import javax.sound.sampled.Clip;
+//import javax.swing.JOptionPane;
+import ProGen.Msg;
+
+public class Main {
+	/*
+	 * Main Class that will actually run the Problem Generator
+	 */
+	
+	//Method that calls the MSG method
+	public static void main(String[] args) {
+	    		MSG();
+	}
+	
+	//Method creates a Msg object, thus starting/creating the Generator
+	public static void MSG() {
+		Msg m= new Msg();
+	}
+	
+}
